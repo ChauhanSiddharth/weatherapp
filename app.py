71c9900a17431a965d22e69907018df4c6e7459f
ChauhanSiddharth <siddharth.chauhan50@yahoo.com>
2020-04-22 20:04:44 -0400

    delete city

--- app.py
+++ app.py
@@ -13,16 +13,15 @@ class City(db.Model):
 	name = db.Column(db.String(50), nullable=False)
 
 @app.route('/', methods=['GET','POST'])
-
-
 def index():
 	if request.method == 'POST':
 		new_city = request.form.get('city')
-		if new_city: 
+		if new_city:
 			new_city_obj = City(name=new_city)
 			db.session.add(new_city_obj)
 			db.session.commit()
 	cities = City.query.all()
+
 	url = 'https://api.openweathermap.org/data/2.5/find?q={}&units=imperial&appid=cba3691acaefbc861932c349b69152ec'
 
 	weather_data = []
@@ -34,15 +33,25 @@ def index():
 			'city' : city,
 			'temperature' : r['list'][0]['main']['temp'],
 			'description' : r['list'][0]['weather'][0]['description'],
-			'icon' : r['list'][0]['weather'][0]['icon'], 
+			'icon' : r['list'][0]['weather'][0]['icon'],
 		}
 
 		weather_data.append(weather)
-	
+
 	#print(weather_data)
 
 	return render_template('weather.html', weather_data=weather_data)
 
+@app.route('/delete', methods=['GET'])
+def delete_data():
+        num_rows_deleted = db.session.query(City).delete()
+        db.session.commit()
+        cities = City.query.all()
+        if cities:
+                app.make_response("Records found")
+        else:
+                return redirect('/')
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
-
